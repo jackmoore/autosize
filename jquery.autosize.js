2064bd7d93a3f29cb6e92e9cd2aeb15a7aca9461
Jack Moore <jack@colorpowered.com>
2011-11-10 00:13:01 -0500

    Fixed the IE9 event detection that was broken in the previous update.

--- jquery.autosize.js
+++ jquery.autosize.js
@@ -1,4 +1,4 @@
-// Autosize 1.1 - jQuery plugin for textareas
+// Autosize 1.2 - jQuery plugin for textareas
 // (c) 2011 Jack Moore - jacklmoore.com
 // license: www.opensource.org/licenses/mit-license.php
 
@@ -7,6 +7,7 @@
 
         // line-height was omitted because IE7/IE8 doesn't return the correct value.
         var 
+        hidden = 'hidden',
         copy = '<textarea style="position:absolute; top:-9999px; left:-9999px; right:auto; bottom:auto; word-wrap:break-word; height:0 !important; min-height:0 !important; overflow:hidden">',
         copyStyle = [
             'fontFamily',
@@ -22,18 +23,20 @@
         return this.each(function () {
             var 
             ta = this,
-            $ta = $(ta),
+            $ta = $(ta).css({
+                overflow: hidden, 
+                overflowY: hidden, 
+                wordWrap: 'break-word'
+            }),
             mirror = $(copy).addClass(className || 'autosizejs')[0],
             minHeight = $ta.height(),
             maxHeight = parseInt($ta.css('maxHeight'), 10) || 9e4,
-            hidden = 'hidden',
             active;
 
             // Using mainly bare JS in this function because it is going
             // to fire very often while typing, and needs to very efficient.
             function adjust() {
                 var height, overflow;
-
                 // the active flag keeps IE from tripping all over itself.  Otherwise
                 // actions in the adjust function will cause IE to call adjust again.
                 if (!active) {
@@ -83,16 +86,28 @@
             // This gives a cross-browser supported way getting the actual
             // height of the text, through the scrollTop property.
             while (i--) {
-                mirror.style[copy[i]] = $ta.css(copy[i]);
+                mirror.style[copyStyle[i]] = $ta.css(copyStyle[i]);
             }
 
             $('body').append(mirror);
 
             $(window).resize(adjust);
 
-            $ta
-                .css({minHeight: minHeight, overflow: hidden, overflowY: hidden, wordWrap: 'break-word'})
-                .bind('onpropertychange' in ta ? 'propertychange' : 'input', adjust);
+            if ('onpropertychange' in ta) {
+                if ('oninput' in ta) {
+                    // This catches IE9.  It does not fire onpropertychange or oninput for deletions,
+                    // so binding to onkeyup to catch most of those occassions.  There is no way that I
+                    // know of to detect something like 'cut' in IE9.
+                    ta.oninput = adjust;
+                    ta.onkeyup = adjust;
+                } else {
+                    // IE7 / IE8
+                    ta.onpropertychange = adjust;
+                }
+            } else {
+                // Modern Browsers
+                ta.oninput = adjust;
+            }
 
             // Call adjust in case the textarea already contains text.
             adjust();
