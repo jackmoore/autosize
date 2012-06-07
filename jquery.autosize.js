ca781e7dbbea38c6384d2c3cfb237942fd37c363
Jack Moore <jack@colorpowered.com>
2012-06-07 19:52:01 -0400

    Added conditional so that autosize cannot be applied twice to the same element When autosize is applied to an element, it will have a data property that links it to the mirrored textarea element. This will make it easier to keep track of and remove unneeded mirror elements.

--- jquery.autosize.js
+++ jquery.autosize.js
@@ -1,134 +1,144 @@
-// Autosize 1.7 - jQuery plugin for textareas
+// Autosize 1.8 - jQuery plugin for textareas
 // (c) 2011 Jack Moore - jacklmoore.com
 // license: www.opensource.org/licenses/mit-license.php
 
 (function ($, undefined) {
-    var 
-    hidden = 'hidden',
-    copy = '<textarea style="position:absolute; top:-9999px; left:-9999px; right:auto; bottom:auto; box-sizing:content-box; word-wrap:break-word; height:0 !important; min-height:0 !important; overflow:hidden">',
-    // line-height is omitted because IE7/IE8 doesn't return the correct value.
-    copyStyle = [
-        'fontFamily',
-        'fontSize',
-        'fontWeight',
-        'fontStyle',
-        'letterSpacing',
-        'textTransform',
-        'wordSpacing'
-    ],
-    oninput = 'oninput',
-    onpropertychange = 'onpropertychange',
-    test = $(copy)[0];
+	var 
+	hidden = 'hidden',
+	copy = '<textarea style="position:absolute; top:-9999px; left:-9999px; right:auto; bottom:auto; box-sizing:content-box; word-wrap:break-word; height:0 !important; min-height:0 !important; overflow:hidden">',
+	// line-height is omitted because IE7/IE8 doesn't return the correct value.
+	copyStyle = [
+		'fontFamily',
+		'fontSize',
+		'fontWeight',
+		'fontStyle',
+		'letterSpacing',
+		'textTransform',
+		'wordSpacing'
+	],
+	oninput = 'oninput',
+	onpropertychange = 'onpropertychange',
+	test = $(copy)[0];
 
-    test.setAttribute(oninput, "return");
+	test.setAttribute(oninput, "return");
 
-    if ($.isFunction(test[oninput]) || onpropertychange in test) {
-        $.fn.autosize = function (className) {
-            return this.each(function () {
-                var 
-                ta = this,
-                $ta = $(ta).css({
-                    overflow: hidden, 
-                    overflowY: hidden, 
-                    wordWrap: 'break-word'
-                }),
-                mirror = $(copy).addClass(className || 'autosizejs')[0],
-                minHeight = $ta.height(),
-                maxHeight = parseInt($ta.css('maxHeight'), 10),
-                active,
-                i = copyStyle.length,
-                boxOffset = $ta.css('box-sizing') === 'border-box' ? $ta.outerHeight() - $ta.height() : 0
+	if ($.isFunction(test[oninput]) || onpropertychange in test) {
+		$.fn.autosize = function (className) {
+			return this.each(function () {
+				var 
+				ta = this,
+				$ta = $(ta),
+				mirror,
+				minHeight = $ta.height(),
+				maxHeight = parseInt($ta.css('maxHeight'), 10),
+				active,
+				i = copyStyle.length,
+				resize,
+				boxOffset = $ta.css('box-sizing') === 'border-box' ? $ta.outerHeight() - $ta.height() : 0;
 
-                // Opera returns '-1px' when max-height is set to 'none'.
-                maxHeight = maxHeight && maxHeight > 0 ? maxHeight : 9e4;
+				if ($ta.data('mirror') || $ta.data('ismirror')) {
+					// if autosize has already been applied, exit.
+					// if autosize is being applied to a mirror element, exit.
+					return;
+				} else {
+					mirror = $(copy).data('ismirror', true).addClass(className || 'autosizejs')[0];
 
-                // Using mainly bare JS in this function because it is going
-                // to fire very often while typing, and needs to very efficient.
-                function adjust() {
-                    var height, overflow;
-                    // the active flag keeps IE from tripping all over itself.  Otherwise
-                    // actions in the adjust function will cause IE to call adjust again.
-                    if (!active) {
-                        active = true;
+					resize = $ta.css('resize') === 'none' ? 'none' : 'horizontal';
 
-                        mirror.value = ta.value;
+					$ta.data('mirror', $(mirror)).css({
+						overflow: hidden, 
+						overflowY: hidden, 
+						wordWrap: 'break-word',
+						resize: resize
+					});
+				}
 
-                        mirror.style.overflowY = ta.style.overflowY;
+				// Opera returns '-1px' when max-height is set to 'none'.
+				maxHeight = maxHeight && maxHeight > 0 ? maxHeight : 9e4;
 
-                        // Update the width in case the original textarea has
-                        // a percent based width, which could change at any time.
-                        mirror.style.width = $ta.css('width');
+				// Using mainly bare JS in this function because it is going
+				// to fire very often while typing, and needs to very efficient.
+				function adjust() {
+					var height, overflow;
+					// the active flag keeps IE from tripping all over itself.  Otherwise
+					// actions in the adjust function will cause IE to call adjust again.
+					if (!active) {
+						active = true;
 
+						mirror.value = ta.value;
 
-                        // Needed for IE to reliably return the correct scrollHeight
-                        mirror.scrollTop = 0;
+						mirror.style.overflowY = ta.style.overflowY;
 
-                        // Set a very high value for scrollTop to be sure the 
-                        // mirror is scrolled all the way to the bottom.
-                        mirror.scrollTop = 9e4;
+						// Update the width in case the original textarea width has changed
+						mirror.style.width = $ta.css('width');
 
-                        height = mirror.scrollTop;
-                        overflow = hidden;
-                        if (height > maxHeight) {
-                            height = maxHeight;
-                            overflow = 'scroll';
-                        } else if (height < minHeight) {
-                            height = minHeight;
-                        }
-                        ta.style.overflowY = overflow;
+						// Needed for IE to reliably return the correct scrollHeight
+						mirror.scrollTop = 0;
 
-                        // Setting the minHeight and maxHeight will restrict webkit resize 
-                        // to only resizing the width.
-                        ta.style.height = ta.style.minHeight = ta.style.maxHeight = height + boxOffset + 'px';
-                        
-                        // This small timeout gives IE a chance to draw it's scrollbar
-                        // before adjust can be run again (prevents an infinite loop).
-                        setTimeout(function () {
-                            active = false;
-                        }, 1);
-                    }
-                }
+						// Set a very high value for scrollTop to be sure the 
+						// mirror is scrolled all the way to the bottom.
+						mirror.scrollTop = 9e4;
 
-                // mirror is a duplicate textarea located off-screen that
-                // is automatically updated to contain the same text as the 
-                // original textarea.  mirror always has a height of 0.
-                // This gives a cross-browser supported way getting the actual
-                // height of the text, through the scrollTop property.
-                while (i--) {
-                    mirror.style[copyStyle[i]] = $ta.css(copyStyle[i]);
-                }
+						height = mirror.scrollTop;
+						overflow = hidden;
+						if (height > maxHeight) {
+							height = maxHeight;
+							overflow = 'scroll';
+						} else if (height < minHeight) {
+							height = minHeight;
+						}
+						ta.style.overflowY = overflow;
 
-                $('body').append(mirror);
+						ta.style.height = height + boxOffset + 'px';
+						
+						// This small timeout gives IE a chance to draw it's scrollbar
+						// before adjust can be run again (prevents an infinite loop).
+						setTimeout(function () {
+							active = false;
+						}, 1);
+					}
+				}
 
-                if (onpropertychange in ta) {
-                    if (oninput in ta) {
-                        // Detects IE9.  IE9 does not fire onpropertychange or oninput for deletions,
-                        // so binding to onkeyup to catch most of those occassions.  There is no way that I
-                        // know of to detect something like 'cut' in IE9.
-                        ta[oninput] = ta.onkeyup = adjust;
-                    } else {
-                        // IE7 / IE8
-                        ta[onpropertychange] = adjust;
-                    }
-                } else {
-                    // Modern Browsers
-                    ta[oninput] = adjust;
-                }
+				// mirror is a duplicate textarea located off-screen that
+				// is automatically updated to contain the same text as the 
+				// original textarea.  mirror always has a height of 0.
+				// This gives a cross-browser supported way getting the actual
+				// height of the text, through the scrollTop property.
+				while (i--) {
+					mirror.style[copyStyle[i]] = $ta.css(copyStyle[i]);
+				}
 
-                $(window).resize(adjust);
+				$('body').append(mirror);
 
-                // Allow for manual triggering if needed.
-                $ta.bind('autosize', adjust);
+				if (onpropertychange in ta) {
+					if (oninput in ta) {
+						// Detects IE9.  IE9 does not fire onpropertychange or oninput for deletions,
+						// so binding to onkeyup to catch most of those occassions.  There is no way that I
+						// know of to detect something like 'cut' in IE9.
+						ta[oninput] = ta.onkeyup = adjust;
+					} else {
+						// IE7 / IE8
+						ta[onpropertychange] = adjust;
+					}
+				} else {
+					// Modern Browsers
+					ta[oninput] = adjust;
+				}
 
-                // Call adjust in case the textarea already contains text.
-                adjust();
-            });
-        }; 
-    } else {
-        // Makes no changes for older browsers (FireFox3- and Safari4-)
-        $.fn.autosize = function () {
-            return this;
-        };
-    }
+				$(window).resize(adjust);
+
+				// Allow for manual triggering if needed.
+				$ta.bind('autosize', adjust);
+
+				// Call adjust in case the textarea already contains text.
+				adjust();
+			});
+		}; 
+	} else {
+		// Makes no changes for older browsers (FireFox3- and Safari4-)
+		$.fn.autosize = function () {
+			return this;
+		};
+	}
 
 }(jQuery));
--- readme.md
+++ readme.md
@@ -4,6 +4,12 @@ Small jQuery plugin to allow dynamic resizing of textarea height, so that it gro
 
 ## Changelog
 
+### Version 1.8 - June 7, 2012
+* Added conditional so that autosize cannot be applied twice to the same element
+* When autosize is applied to an element, it will have a data property that links it to the mirrored textarea element.  This will make it easier to keep track of and remove unneeded mirror elements.  Example:
+    $('textarea.example').data('mirror').remove(); // delete the mirror
+    $('textarea.example').remove(); // delete the original
+
 ### Version 1.7 - May 3, 2012
 * Now supports box-sizing:border-box
 
