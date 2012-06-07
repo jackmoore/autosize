7b4832e296526eb69d1f60e272e28588a3d55e6c
Alexander Madyankin <alexander@madyankin.name>
2012-06-07 14:44:41 +0600

    Double-apply bugfix

--- jquery.autosize.js
+++ jquery.autosize.js
@@ -25,6 +25,9 @@
     if ($.isFunction(test[oninput]) || onpropertychange in test) {
         $.fn.autosize = function (className) {
             return this.each(function () {
+                if ($(this).data("used") == true) return;
+                $(this).data("used", true);
+
                 var 
                 ta = this,
                 $ta = $(ta).css({
