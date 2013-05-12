34b2c45e64f6f0b00dfe2be6f021799b6785135e
pleasurazy <pleasurazy@gmail.com>
2013-05-13 00:45:02 +0800

    added AngularJs support

--- angularjs/directive.js
+++ angularjs/directive.js
@@ -1,1 +1,11 @@
 ;'use strict';
+angular.module('textarea-autosize', []).directive('autosize', function factory() {
+  return {
+      restrict: 'A',
+      compile: function(elem, attrs, transcludeFn) {
+          return function link (scope, element, attrs) {
+            element.autosize(scope.$eval(attrs.autosize));
+          };
+      }
+  };
+});
