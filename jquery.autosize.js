c79c5c95ce857c80908f0cb7ce3cf2520c2bfe2e
Jack Moore <hello@jacklmoore.com>
2013-07-30 22:46:38 -0400

    passing contributor commit through build step.

--- jquery.autosize.js
+++ jquery.autosize.js
@@ -1,5 +1,5 @@
 /*!
-	Autosize v1.17.2 - 2013-07-28
+	Autosize v1.17.2 - 2013-07-30
 	Automatically adjust textarea height based on user input.
 	(c) 2013 Jack Moore - http://www.jacklmoore.com/autosize
 	license: http://www.opensource.org/licenses/mit-license.php
