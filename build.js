d8bf2a3f40f5d188a899c87a7e982d2d4de48e70
Jack Moore <hello@jacklmoore.com>
2015-02-26 20:41:41 -0500

    added main field

--- build.js
+++ build.js
@@ -15,7 +15,7 @@ function writeBower() {
 		homepage: pkg.homepage,
 		ignore: pkg.config.bower.ignore,
 		repository: pkg.repository,
-		main: pkg.config.bower.main,
+		main: pkg.config.main,
 	};
 	fs.writeFile('bower.json', JSON.stringify(bower, null, '\t'));
 	return true;
