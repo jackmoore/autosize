f42f4d053bf054c91b8ca44f9330878cefd2f933
Mauro Fujihara <fujiharam@isao.co.jp>
2018-03-20 14:41:07 +0900

    Workaround for iOS 11

--- src/autosize.js
+++ src/autosize.js
@@ -119,10 +119,12 @@ function assign(ta) {
 		// used to check if an update is actually necessary on window.resize
 		clientWidth = ta.clientWidth;
 
-		// prevents scroll-position jumping
-		overflows.forEach(el => {
-			el.node.scrollTop = el.scrollTop
-		});
+        /* This commented out code breaks causes the keyboard to overlay the input box on IOS 11
+        // prevents scroll-position jumping
+        overflows.forEach(el => {
+        	el.node.scrollTop = el.scrollTop
+        });
+        */
 
 		if (docTop) {
 			document.documentElement.scrollTop = docTop;
